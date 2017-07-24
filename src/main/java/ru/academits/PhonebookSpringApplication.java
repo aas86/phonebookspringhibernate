6595971b58d848da31e3c465e7302ca91c0ad4ff
Anna Usova <usova@improveitgroup.com>
2017-07-24 16:43:59 +0700

    sheduler

--- src/main/java/ru/academits/PhonebookSpringApplication.java
+++ src/main/java/ru/academits/PhonebookSpringApplication.java
@@ -2,8 +2,10 @@ package ru.academits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PhonebookSpringApplication {
 
 	public static void main(String[] args) {
